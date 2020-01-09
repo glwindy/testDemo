6c9c186ecd6c4b762579413d2746441f48d261f5
pangcaiping <980178683@qq.com>
2020-01-09 16:07:19 +0800

    feat: 初始化代码

---
+++ src/main/java/com/test/demo/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.test.demo.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@Api(tags = "测试controller")
+public class TestController {
+
+    @ApiOperation("测试获取值")
+    @GetMapping("/getAA")
+    public String getTest() {
+        return "This is a test";
+    }
+}
